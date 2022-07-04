0a3cf8bd1c7048522d903cfcdce68c74fba46afe
King-agana <107753153+King-agana@users.noreply.github.com>
2022-07-04 10:21:20 +0100

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTER</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header class="header">
+    <section class="flex">
+        <h2><img src="4logo.jpg" alt="logo"></h2>
+        <nav class="navbar">
+            <a href="login.html">Login</a>
+            <a href="signup.html" >Registration</a>
+            <a href="reset.html">Contact</a>
+        </nav>
+        <div id="menu-btn" style="@media(max-width)" class="fas fa-bars"></div>
+        
+    </section>
+</header>
+    <header class="hed" >
+      <h1>SIGN UP</h1>
+    </header>
+
+    <main class="mainn">
+        <form>
+            
+              <p><label for="name">Name <em>Have an accout already?<a href="login.html">login</a></em></label></p>
+              <input type="text" name="First-name" class="matt" required placeholder="First name"><br>
+              <label for="email">Email</label><br>
+              <input type="email" class="matt" required placeholder="Email address"><br>
+            <label for="Country">Country</label><br>
+             <input type="text" name="First-name" class="matt" id=""required  placeholder="Your country"><br>
+              <label for="password">password</label><br>
+              <input type="password" name="password" id="" class="matt"  required placeholder="Password"><br>
+              <label for="password">Confirm password</label><br>
+              <input type="co-password" name="First-name" id="" class="matt"  required placeholder="Confirm password"><br>
+              <button type="submit">signup</button>
+            </form>
+    </main>
+</body>
+</html>
